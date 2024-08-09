4d04deaf8c662551a50d64bdb90fb1ece2b9c5bd
Matheus Reis <matheuss_reis@outlook.com>
2024-08-08 23:16:36 -0300

    Bytebuffer refeito para usar o Buffer do Websocket

--- src/network/buffers/byte_buffer.ts
+++ src/network/buffers/byte_buffer.ts
@@ -1,91 +1,80 @@
-import { ArrayBufferSink } from "bun";
+import { Buffer } from "node:buffer";
 
 export class ByteBuffer {
-    private _sink: ArrayBufferSink = new ArrayBufferSink();
-    private _buffer: Uint8Array = new Uint8Array();
+    private _buffer: Buffer;
+    private _offset: number;
 
     constructor() {
-        this._sink.start({ stream: true, asUint8Array: true });
+        this._buffer = Buffer.alloc(0);
+        this._offset = 0;
     }
 
-    public putBytes(bytes: Uint8Array): void {
-        this._sink.write(bytes);
+    public putBytes(bytes: Buffer): void {
+        const newSize = this._buffer.length + bytes.length;
+        const newBuffer = Buffer.alloc(newSize);
+        this._buffer.copy(newBuffer);
+        bytes.copy(newBuffer, this._buffer.length);
+        this._buffer = newBuffer;
     }
 
     public putInt8(value: number): void {
-        if (value < -128 || value > 127) {
-            throw new RangeError("Value out of range for Int8");
-        }
-        const buffer = new Uint8Array(1);
-        buffer[0] = value;
-        this._sink.write(buffer);
+        const buffer = Buffer.alloc(1);
+        buffer.writeInt8(value, 0);
+        this.putBytes(buffer);
     }
 
     public putInt16(value: number): void {
-        if (value < -32768 || value > 32767) {
-            throw new RangeError("Value out of range for Int16");
-        }
-        const buffer = new DataView(new ArrayBuffer(2));
-        buffer.setInt16(0, value, true);
-        this._sink.write(new Uint8Array(buffer.buffer));
+        const buffer = Buffer.alloc(2);
+        buffer.writeInt16LE(value, 0);
+        this.putBytes(buffer);
     }
 
     public putInt32(value: number): void {
-        if (value < -2147483648 || value > 2147483647) {
-            throw new RangeError("Value out of range for Int32");
-        }
-        const buffer = new DataView(new ArrayBuffer(4));
-        buffer.setInt32(0, value, true);
-        this._sink.write(new Uint8Array(buffer.buffer));
+        const buffer = Buffer.alloc(4);
+        buffer.writeInt32LE(value, 0);
+        this.putBytes(buffer);
     }
 
     public putString(value: string): void {
-        const encoder = new TextEncoder();
-        const encoded = encoder.encode(value);
+        const encoded = Buffer.from(value, 'utf8');
         this.putInt32(encoded.length);
         this.putBytes(encoded);
     }
 
-    public flush(): void {
-        this._buffer = new Uint8Array(this._sink.flush() as ArrayBuffer);
-    }
-
-    private _readBuffer(size: number): Uint8Array {
-        if (size > this._buffer.length) {
-            throw new RangeError("Requested size exceeds buffer length");
-        }
-        const buffer = this._buffer.slice(0, size);
-        this._buffer = this._buffer.slice(size);
-        return buffer;
-    }
-
-    public getBytes(): Uint8Array {
-        this.flush();
-        return this._buffer;
-    }
-
     public getInt8(): number {
-        const buffer: Uint8Array = this._readBuffer(1);
-        const view = new DataView(buffer.buffer);
-        return view.getInt8(0);
+        const value = this._buffer.readInt8(this._offset);
+        this._offset += 1;
+        return value;
     }
 
     public getInt16(): number {
-        const buffer = this._readBuffer(2);
-        const view = new DataView(buffer.buffer);
-        return view.getInt16(0, true);
+        const value = this._buffer.readInt16LE(this._offset);
+        this._offset += 2;
+        return value;
     }
 
     public getInt32(): number {
-        const buffer = this._readBuffer(4);
-        const view = new DataView(buffer.buffer);
-        return view.getInt32(0, true);
+        const value = this._buffer.readInt32LE(this._offset);
+        this._offset += 4;
+        return value;
     }
 
     public getString(): string {
         const length = this.getInt32();
-        const buffer = this._readBuffer(length);
-        const decoder = new TextDecoder();
-        return decoder.decode(buffer);
+        const value = this._buffer.slice(this._offset, this._offset + length);
+        this._offset += length;
+        return value.toString('utf8');
+    }
+
+    public getBuffer(): Buffer {
+        return this._buffer;
+    }
+
+    public getOffset(): number {
+        return this._offset;
+    }
+
+    public setOffset(offset: number): void {
+        this._offset = offset;
     }
 }
